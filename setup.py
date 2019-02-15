e54910df58162c6a68cba83a1d7e9fd95bf10d21
iver56 <ij@aiascience.com>
2019-02-15 09:38:59 +0100

    Add more properties to setup.py to prepare for PyPI upload

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
 setup(
     name="audiomentations",
-    version="0.1",
-    packages=find_packages(exclude=["tests"]),
+    version="0.1.0",
+    author="Iver Jordal",
+    description="A library for audio data augmentation. Inspired by albumentations.",
     license="MIT",
-    long_description=open("README.md").read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/iver56/audiomentations",
+    packages=find_packages(exclude=["tests"]),
     install_requires=["numpy>=1.13.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+    ],
 )

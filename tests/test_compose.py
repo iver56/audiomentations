4e73e6fcda808d3b7150792ba3c69b84d6b0b79e
iver56 <ij@aiascience.com>
2019-02-18 12:33:16 +0100

    Test TimeStretch

--- tests/test_compose.py
+++ tests/test_compose.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from audiomentations.augmentations.transforms import AddGaussianNoise
+from audiomentations.augmentations.transforms import AddGaussianNoise, TimeStretch
 from audiomentations.core.composition import Compose
 
 
@@ -15,4 +15,14 @@ class TestCompose(unittest.TestCase):
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertNotAlmostEqual(float(np.sum(np.abs(samples))), 0.0)
+
+    def test_time_stretch(self):
+        samples = np.zeros((20,), dtype=np.float32)
+        sample_rate = 16000
+        augmenter = Compose([
+            TimeStretch(min_rate=0.8, max_rate=0.9, p=1.0)
+        ])
+        samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertGreater(len(samples), 20)
 

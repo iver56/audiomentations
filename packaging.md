e83e4e002b216b8bd06da5ec806de3d5ab3249db
iver56 <ij@aiascience.com>
2019-02-18 12:35:52 +0100

    Release v0.2.0

---
+++ packaging.md
@@ -0,0 +1,3 @@
+* Bump version number in setup.py
+* `python setup.py sdist bdist_wheel`
+* `python -m twine upload dist/*`
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="audiomentations",
-    version="0.1.0",
+    version="0.2.0",
     author="Iver Jordal",
     description="A library for audio data augmentation. Inspired by albumentations.",
     license="MIT",

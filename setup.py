c1818623ff18461ca5293b23ef34aa948b0ed504
iver56 <iver56@hotmail.com>
2019-02-19 19:57:18 +0100

    Release v0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme_file:
 
 setup(
     name="audiomentations",
-    version="0.3.0",
+    version="0.4.0",
     author="Iver Jordal",
     description="A library for audio data augmentation. Inspired by albumentations.",
     license="MIT",

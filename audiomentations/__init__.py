82f8631671d8bb40f3227c6e1ae77938a4122d78
iver56 <ij@aiascience.com>
2019-03-05 10:11:38 +0100

    Reformat code with black (to trigger a build)

--- audiomentations/__init__.py
+++ audiomentations/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1'
+__version__ = "0.1"
 
 from .core.composition import *
 from .core.transforms_interface import *
--- tests/test_time_stretch.py
+++ tests/test_time_stretch.py
@@ -10,9 +10,9 @@ class TestTimeStretch(unittest.TestCase):
     def test_dynamic_length(self):
         samples = np.zeros((20,), dtype=np.float32)
         sample_rate = 16000
-        augmenter = Compose([
-            TimeStretch(min_rate=0.8, max_rate=0.9, leave_length_unchanged=False,  p=1.0)
-        ])
+        augmenter = Compose(
+            [TimeStretch(min_rate=0.8, max_rate=0.9, leave_length_unchanged=False, p=1.0)]
+        )
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertEqual(samples.dtype, np.float32)
@@ -21,11 +21,10 @@ class TestTimeStretch(unittest.TestCase):
     def test_fixed_length(self):
         samples = np.zeros((20,), dtype=np.float32)
         sample_rate = 16000
-        augmenter = Compose([
-            TimeStretch(min_rate=0.8, max_rate=0.9, leave_length_unchanged=True,  p=1.0)
-        ])
+        augmenter = Compose(
+            [TimeStretch(min_rate=0.8, max_rate=0.9, leave_length_unchanged=True, p=1.0)]
+        )
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertEqual(samples.dtype, np.float32)
         self.assertEqual(len(samples), 20)
-

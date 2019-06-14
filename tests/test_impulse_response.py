b51fcaf6849acd0aaaf46ac9a448469402a8a118
Nikolay Karpov <karpnv@gmail.com>
2019-06-14 20:36:06 +0300

    Add tests

---
+++ tests/test_impulse_response.py
@@ -0,0 +1,20 @@
+import unittest
+
+import numpy as np
+
+from audiomentations.augmentations.transforms import AddImpulseResponse
+from audiomentations.core.composition import Compose
+
+
+class TestImpulseResponse(unittest.TestCase):
+    def test_dynamic_length(self):
+        sample_len = 1024
+        samples_in = np.random.normal(0, 1, size=sample_len).astype(np.float32)
+        sample_rate = 16000
+        augmenter = Compose([AddImpulseResponse(ir_path="../demo/ir", p=1.0)])
+
+        self.assertEqual(len(samples_in), sample_len)
+        samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)
+
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertGreater(len(samples_out), len(samples_in))
---
+++ tests/test_trim.py
@@ -0,0 +1,21 @@
+import unittest
+
+import numpy as np
+
+from audiomentations.augmentations.transforms import Trim
+from audiomentations.core.composition import Compose
+
+
+class TestTrim(unittest.TestCase):
+    def test_dynamic_length(self):
+        sample_len = 1024
+        samples1 = np.zeros((sample_len,), dtype=np.float32)
+        samples2 = np.random.normal(0, 1, size=sample_len).astype(np.float32)
+        sample_rate = 16000
+        augmenter = Compose([Trim(top_db=20, p=1.0)])
+        samples_in = np.hstack((samples1, samples2))
+        self.assertEqual(len(samples_in), sample_len * 2)
+        samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)
+
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertLess(len(samples_out), sample_len * 2)

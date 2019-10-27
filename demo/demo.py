24986ae915b2c9a5b007e4ffdcd585aad2141c96
askskro <ask.krogstad@gmail.com>
2019-10-27 20:48:14 +0100

    Remove Downsample transformation

--- demo/demo.py
+++ demo/demo.py
@@ -15,7 +15,6 @@ from audiomentations import (
     TimeMask,
     AddGaussianSNR,
     Resample,
-    Downsample,
 )
 
 SAMPLE_RATE = 16000
@@ -125,12 +124,5 @@ if __name__ == "__main__":
     augmenter = Compose([Resample(p=1.0)])
     for i in range(5):
         output_file_path = os.path.join(output_dir, "Resample_{:03d}.wav".format(i))
-        augmented_samples = augmenter(samples=samples, sample_rate=SAMPLE_RATE)
-        wavfile.write(output_file_path, rate=SAMPLE_RATE, data=augmented_samples)
-
-    # Downsample
-    augmenter = Compose([Downsample(p=1.0)])
-    for i in range(5):
-        output_file_path = os.path.join(output_dir, "Downsample_{:03d}.wav".format(i))
         augmented_samples = augmenter(samples=samples, sample_rate=SAMPLE_RATE)
         wavfile.write(output_file_path, rate=SAMPLE_RATE, data=augmented_samples)

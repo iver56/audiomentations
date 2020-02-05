55de839f3fbded6171809b36672f0e70b47c5beb
iver56 <ij@aiascience.com>
2020-02-05 13:13:49 +0100

    Test the freeze_parameters property

--- tests/test_pitch_shift.py
+++ tests/test_pitch_shift.py
@@ -1,6 +1,8 @@
 import unittest
+from copy import deepcopy
 
 import numpy as np
+from numpy.testing import assert_array_equal
 
 from audiomentations.augmentations.transforms import PitchShift
 from audiomentations.core.composition import Compose
@@ -15,3 +17,23 @@ class TestPitchShift(unittest.TestCase):
 
         self.assertEqual(samples.dtype, np.float32)
         self.assertEqual(len(samples), 512)
+
+    def test_freeze_parameters(self):
+        """
+        Test that the transform can freeze its parameters, e.g. to apply the effect with the
+        same parameters to multiple sounds.
+        """
+        samples = np.sin(np.linspace(0, 440 * 2 * np.pi, 8000)).astype(np.float32)
+        sample_rate = 16000
+        augmenter = Compose([PitchShift(min_semitones=1, max_semitones=12, p=1.0)])
+
+        first_samples = augmenter(samples=samples, sample_rate=sample_rate)
+        first_parameters = deepcopy(augmenter.transforms[0].parameters)
+
+        augmenter.transforms[0].min_semitones = -12
+        augmenter.transforms[0].max_semitones = -1
+        augmenter.transforms[0].freeze_parameters = True
+        second_samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(first_parameters, augmenter.transforms[0].parameters)
+        assert_array_equal(first_samples, second_samples)

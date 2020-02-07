6534b5c75262ceef6135966557a5291a67fd9bba
iver56 <ij@aiascience.com>
2020-02-07 10:50:37 +0100

    Add Shift without rollover to the demo script

--- demo/demo.py
+++ demo/demo.py
@@ -115,6 +115,13 @@ if __name__ == "__main__":
         augmented_samples = augmenter(samples=samples, sample_rate=SAMPLE_RATE)
         wavfile.write(output_file_path, rate=SAMPLE_RATE, data=augmented_samples)
 
+    # Shift without rollover
+    augmenter = Compose([Shift(min_fraction=-0.5, max_fraction=0.5, rollover=False, p=1.0)])
+    for i in range(5):
+        output_file_path = os.path.join(output_dir, "ShiftWithoutRollover_{:03d}.wav".format(i))
+        augmented_samples = augmenter(samples=samples, sample_rate=SAMPLE_RATE)
+        wavfile.write(output_file_path, rate=SAMPLE_RATE, data=augmented_samples)
+
     # Normalize
     augmenter = Compose([Normalize(p=1.0)])
     output_file_path = os.path.join(output_dir, "Normalize_{:03d}.wav".format(0))

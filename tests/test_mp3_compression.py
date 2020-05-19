636e17aa8342b59a04dbec9915dda715d205e3ea
iver56 <ij@aiascience.com>
2020-05-19 11:10:22 +0200

    Test medium and low bitrates consistently

--- tests/test_mp3_compression.py
+++ tests/test_mp3_compression.py
@@ -11,8 +11,20 @@ class TestMp3Compression(unittest.TestCase):
         sample_len = 44100
         samples_in = np.random.normal(0, 1, size=sample_len).astype(np.float32)
         sample_rate = 44100
-        augmenter = Compose([Mp3Compression(p=1.0)])
+        augmenter = Compose([Mp3Compression(p=1.0, min_bitrate=48, max_bitrate=48)])
 
         samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)
         self.assertEqual(samples_out.dtype, np.float32)
         self.assertGreaterEqual(len(samples_out), sample_len)
+        self.assertLess(len(samples_out), sample_len + 2500)
+
+    def test_apply_mp3_compression_low_bitrate(self):
+        sample_len = 16000
+        samples_in = np.random.normal(0, 1, size=sample_len).astype(np.float32)
+        sample_rate = 16000
+        augmenter = Compose([Mp3Compression(p=1.0, min_bitrate=8, max_bitrate=8)])
+
+        samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertGreaterEqual(len(samples_out), sample_len)
+        self.assertLess(len(samples_out), sample_len + 2500)

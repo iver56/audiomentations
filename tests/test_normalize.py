5a746774a37e3499dc021323ce8594f3a7da0df9
iver56 <iver56@hotmail.com>
2020-07-27 13:26:44 +0200

    Test Normalize's handling of complete silence (all zeros)

--- tests/test_normalize.py
+++ tests/test_normalize.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from audiomentations.augmentations.transforms import TimeStretch, Normalize
+from audiomentations.augmentations.transforms import Normalize
 from audiomentations.core.composition import Compose
 
 
@@ -24,6 +24,17 @@ class TestNormalize(unittest.TestCase):
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertEqual(np.amin(samples), -1.0)
+        self.assertEqual(samples[-1], 0.0)
+        self.assertEqual(samples.dtype, np.float32)
+        self.assertEqual(len(samples), 4)
+
+    def test_normalize_all_zeros(self):
+        samples = np.array([0.0, 0.0, 0.0, 0.0], dtype=np.float32)
+        sample_rate = 16000
+        augmenter = Compose([Normalize(p=1.0)])
+        samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(np.amin(samples), 0.0)
         self.assertEqual(samples[-1], 0.0)
         self.assertEqual(samples.dtype, np.float32)
         self.assertEqual(len(samples), 4)

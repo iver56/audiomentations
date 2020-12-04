aea9bf4a3314f8a4dac7006866d3d76be3d5b1be
iver56 <ij@aiascience.com>
2020-12-04 23:20:10 +0100

    Test multichannel TimeStretch

--- tests/test_time_stretch.py
+++ tests/test_time_stretch.py
@@ -3,20 +3,16 @@ import unittest
 import numpy as np
 
 from audiomentations.augmentations.transforms import TimeStretch
-from audiomentations.core.composition import Compose
 
 
 class TestTimeStretch(unittest.TestCase):
     def test_dynamic_length(self):
         samples = np.zeros((20,), dtype=np.float32)
         sample_rate = 16000
-        augmenter = Compose(
-            [
-                TimeStretch(
-                    min_rate=0.8, max_rate=0.9, leave_length_unchanged=False, p=1.0
-                )
-            ]
+        augmenter = TimeStretch(
+            min_rate=0.8, max_rate=0.9, leave_length_unchanged=False, p=1.0
         )
+
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertEqual(samples.dtype, np.float32)
@@ -25,14 +21,26 @@ class TestTimeStretch(unittest.TestCase):
     def test_fixed_length(self):
         samples = np.zeros((20,), dtype=np.float32)
         sample_rate = 16000
-        augmenter = Compose(
-            [
-                TimeStretch(
-                    min_rate=0.8, max_rate=0.9, leave_length_unchanged=True, p=1.0
-                )
-            ]
+        augmenter = TimeStretch(
+            min_rate=0.8, max_rate=0.9, leave_length_unchanged=True, p=1.0
         )
+
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertEqual(samples.dtype, np.float32)
         self.assertEqual(len(samples), 20)
+
+    def test_multichannel(self):
+        num_channels = 3
+        samples = np.random.normal(0, 0.1, size=(num_channels, 5555)).astype(np.float32)
+        sample_rate = 16000
+        augmenter = TimeStretch(
+            min_rate=0.8, max_rate=0.9, leave_length_unchanged=True, p=1.0
+        )
+
+        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(samples.dtype, samples_out.dtype)
+        self.assertEqual(samples.shape, samples_out.shape)
+        for i in range(num_channels):
+            assert not np.allclose(samples[i], samples_out[i])

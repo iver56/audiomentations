4500f8e936bf1e685504ce25cf966c3475ff830d
omerferhatt <omerf.sarioglu@gmail.com>
2021-01-08 18:27:25 +0300

    SpecCompose Implementation - BaseCompose class added - Compose class inherited from BaseCompose - SpecCompose class created according to magnitude spectrogram - SpecCompose unit test added

--- audiomentations/core/composition.py
+++ audiomentations/core/composition.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Compose:
+class BaseCompose:
     def __init__(self, transforms, p=1.0, shuffle=False):
         self.transforms = transforms
         self.p = p
@@ -12,22 +12,14 @@ class Compose:
             name_list.append(type(transform).__name__)
         self.__name__ = "_".join(name_list)
 
-    def __call__(self, samples, sample_rate):
-        transforms = self.transforms.copy()
-        if random.random() < self.p:
-            if self.shuffle:
-                random.shuffle(transforms)
-            for transform in transforms:
-                samples = transform(samples, sample_rate)
+    def __call__(self, *args, **kwargs):
+        raise NotImplementedError
 
-        return samples
-
-    def randomize_parameters(self, samples, sample_rate):
+    def randomize_parameters(self, *args, **kwargs):
         """
         Randomize and define parameters of every transform in composition.
         """
-        for transform in self.transforms:
-            transform.randomize_parameters(samples, sample_rate)  
+        raise NotImplementedError
 
     def freeze_parameters(self):
         """
@@ -44,3 +36,42 @@ class Compose:
         """
         for transform in self.transforms:
             transform.unfreeze_parameters()
+
+
+class Compose(BaseCompose):
+    # TODO: Name can change to WaveformCompose
+    def __init__(self, transforms, p=1.0, shuffle=False):
+        super(Compose, self).__init__(transforms, p, shuffle)
+
+    def __call__(self, samples, sample_rate):
+        transforms = self.transforms.copy()
+        if random.random() < self.p:
+            if self.shuffle:
+                random.shuffle(transforms)
+            for transform in transforms:
+                samples = transform(samples, sample_rate)
+
+        return samples
+
+    def randomize_parameters(self, samples, sample_rate):
+        for transform in self.transforms:
+            transform.randomize_parameters(samples, sample_rate)
+
+
+class SpecCompose(BaseCompose):
+    def __init__(self, transforms, p=1.0, shuffle=False):
+        super().__init__(transforms, p, shuffle)
+
+    def __call__(self, magnitude_spectrogram):
+        transforms = self.transforms.copy()
+        if random.random() < self.p:
+            if self.shuffle:
+                random.shuffle(transforms)
+            for transform in transforms:
+                magnitude_spectrogram = transform(magnitude_spectrogram)
+
+        return magnitude_spectrogram
+
+    def randomize_parameters(self, magnitude_spectrogram):
+        for transform in self.transforms:
+            transform.randomize_parameters(magnitude_spectrogram)

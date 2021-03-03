6c478bcd84589effdc2baec5fe6765599365b830
iver56 <iver56@hotmail.com>
2021-03-03 16:17:26 +0100

    Fix typo ble -> be

--- audiomentations/augmentations/transforms.py
+++ audiomentations/augmentations/transforms.py
@@ -527,7 +527,7 @@ class Resample(BaseWaveformTransform):
 class ClippingDistortion(BaseWaveformTransform):
     """Distort signal by clipping a random percentage of points
 
-    The percentage of points that will ble clipped is drawn from a uniform distribution between
+    The percentage of points that will be clipped is drawn from a uniform distribution between
     the two input parameters min_percentile_threshold and max_percentile_threshold. If for instance
     30% is drawn, the samples are clipped if they're below the 15th or above the 85th percentile.
     """

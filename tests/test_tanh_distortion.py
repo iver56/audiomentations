8670664377182e294d9756070e2733fb07da55b3
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2021-06-11 18:01:47 +0300

    Update test_tanh_distortion.py

--- tests/test_tanh_distortion.py
+++ tests/test_tanh_distortion.py
@@ -7,15 +7,29 @@ from audiomentations.core.composition import Compose
 
 
 class TestTanhDistortion(unittest.TestCase):
-    def test_tanh_distortion(self):
-        sample_len = 1024
-        samples1 = np.zeros((sample_len,), dtype=np.float32)
-        samples2 = np.random.normal(0, 1, size=sample_len).astype(np.float32)
+    def test_single_channel(self):
+        samples = np.zeros((2048,), dtype=np.float32)
         sample_rate = 16000
-        augmenter = Compose([TanhDistortion(c1=2, c2=2)])
-        samples_in = np.hstack((samples1, samples2))
-        self.assertEqual(len(samples_in), sample_len * 2)
-        samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)
+        augmenter = TanhDistortion(
+            c1=2, c2=2, p=1.0
+        )
 
-        self.assertEqual(samples_out.dtype, np.float32)
-        self.assertEqual(len(samples_out), sample_len)
+        samples = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(samples.dtype, np.float32)
+        self.assertEqual(len(samples), 2048)
+
+    def test_multichannel(self):
+        num_channels = 3
+        samples = np.random.normal(0, 0.1, size=(num_channels, 5555)).astype(np.float32)
+        sample_rate = 16000
+        augmenter = TanhDistortion(
+            c1=2, c2=2, p=1.0
+        )
+
+        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+
+        self.assertEqual(samples.dtype, samples_out.dtype)
+        self.assertEqual(samples.shape, samples_out.shape)
+        for i in range(num_channels):
+            assert not np.allclose(samples[i], samples_out[i])

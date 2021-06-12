6406c0df0aee8840c55d2c986ec164337dc9c2ce
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2021-06-12 20:49:00 +0300

    Update test_tanh_distortion.py

--- tests/test_tanh_distortion.py
+++ tests/test_tanh_distortion.py
@@ -9,7 +9,7 @@ from audiomentations.core.utils import calculate_rms
 
 class TestTanhDistortion(unittest.TestCase):
     def test_single_channel(self):
-        samples = np.zeros((2048,), dtype=np.float32)
+        samples = np.random.normal(0, 0.1, size=(2048, )).astype(np.float32) 
         sample_rate = 16000
         augmenter = TanhDistortion(
            min_distortion_gain=1., max_distortion_gain=2.0, p=1.0
@@ -19,8 +19,9 @@ class TestTanhDistortion(unittest.TestCase):
 
         self.assertEqual(samples.dtype, distorted_samples.dtype)
         self.assertEqual(samples.shape, distorted_samples.shape)
-        self.assertEqual(calculate_rms(samples), calculate_rms(distorted_samples))
-
+        self.assertEqual(np.round(calculate_rms(samples), 3), 
+                         np.round(calculate_rms(distorted_samples), 3))
+        
     def test_multichannel(self):
         num_channels = 3
         samples = np.random.normal(0, 0.1, size=(num_channels, 5555)).astype(np.float32)
@@ -33,6 +34,7 @@ class TestTanhDistortion(unittest.TestCase):
 
         self.assertEqual(samples.dtype, distorted_samples.dtype)
         self.assertEqual(samples.shape, distorted_samples.shape)
-        self.assertEqual(calculate_rms(samples), calculate_rms(distorted_samples))
         for i in range(num_channels):
             assert not np.allclose(samples[i], distorted_samples[i])
+            self.assertEqual(np.round(calculate_rms(samples[i]), 3), 
+                             np.round(calculate_rms(distorted_samples[i]), 3))

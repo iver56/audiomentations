153f82dd423de486ba38e77b0204ac1095fabf65
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2021-06-19 17:08:01 +0300

    Create test_band_pass_filter.py

---
+++ tests/test_band_pass_filter.py
@@ -0,0 +1,29 @@
+import unittest
+
+import numpy as np
+
+from audiomentations.augmentations.transforms import BandPassFilter
+
+
+class TestBandPassFilter(unittest.TestCase):
+    def test_band_pass_filter(self):
+        samples = np.array(
+            [
+                [[0.75, 0.5, -0.25, -0.125, 0.0], [0.65, 0.5, -0.25, -0.125, 0.0]],
+                [[0.3, 0.5, -0.25, -0.125, 0.0], [0.9, 0.5, -0.25, -0.125, 0.0]],
+                [[0.9, 0.5, -0.25, -1.06, 0.0], [0.9, 0.5, -0.25, -1.12, 0.0]],
+            ],
+            dtype=np.float32,
+        )
+        sample_rate = 16000
+
+        augment = BandPassFilter(min_low_cutoff_freq=200, 
+                                 max_low_cutoff_freq=7000,
+                                 min_high_cutoff_freq=10000,
+                                 max_high_cutoff_freq=17000,
+                                 p=1.0)
+        processed_samples = augment(
+            samples=samples, sample_rate=sample_rate
+        )
+        self.assertEqual(processed_samples.shape, samples.shape)
+        self.assertEqual(processed_samples.dtype, np.float32)

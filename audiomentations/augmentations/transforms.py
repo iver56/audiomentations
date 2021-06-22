21562088dabf76f4c9b57c176da1b296766537a5
iver56 <iver56@hotmail.com>
2021-06-22 21:19:38 +0200

    Expand TanhDistortion docstring

--- audiomentations/augmentations/transforms.py
+++ audiomentations/augmentations/transforms.py
@@ -1124,7 +1124,15 @@ class Gain(BaseWaveformTransform):
     
 class TanhDistortion(BaseWaveformTransform):
     """
-    Apply tanh distortion to the audio.
+    Apply tanh distortion to the audio. This technique is sometimes used for adding
+    distortion to guitar recordings. The tanh() function can give a rounded
+    "soft clipping" kind of distortion, and the distortion amount is proportional to the
+    loudness of the input and the pre-gain. Tanh is symmetric, so the positive and
+    negative parts of the signal are squashed in the same way. This transform can be
+    useful as data augmentation because it adds harmonics. In other words, it changes
+    the timbre of the sound.
+
+    See this page for examples: http://gdsp.hf.ntnu.no/lessons/3/17/
     """
     
     supports_multichannel = True

a96c9a0ec37e151337eda2e8780916cd8c757c44
iver56 <iver56@hotmail.com>
2021-06-25 12:16:48 +0200

    Add more tests of AddGaussianSNR and cover some more edge cases. Pick random SNR in dB scale. Explain how to migrate from legacy params to new params.

--- audiomentations/augmentations/transforms.py
+++ audiomentations/augmentations/transforms.py
@@ -208,64 +208,92 @@ class AddGaussianSNR(BaseWaveformTransform):
     min_SNR and max_SNR, which had inverse (wrong) characteristics. The use of these
     parameters is discouraged, and one should use min_snr_in_db and max_snr_in_db
     instead now.
+
+    Note also that if you use the new parameters, a random SNR will be picked uniformly
+    in the decibel scale instead of a uniform amplitude ratio. This aligns
+    with human hearing, which is more logarithmic than linear.
     """
 
     supports_multichannel = True
 
     def __init__(
-        self, min_SNR=0.001, max_SNR=1.0, min_snr_in_db=None, max_snr_in_db=None, p=0.5
+        self, min_SNR=None, max_SNR=None, min_snr_in_db=None, max_snr_in_db=None, p=0.5
     ):
         """
-        :param min_snr_in_db: Minimum signal-to-noise ratio in db
-        :param max_snr_in_db: Maximum signal-to-noise ratio in db
-        :param min_SNR: Minimum signal-to-noise ratio (legacy)
-        :param max_SNR: Maximum signal-to-noise ratio (legacy)
+        :param min_SNR: Minimum signal-to-noise ratio (legacy). A lower number means less noise.
+        :param max_SNR: Maximum signal-to-noise ratio (legacy). A greater number means more noise.
+        :param min_snr_in_db: Minimum signal-to-noise ratio in db. A lower number means more noise.
+        :param max_snr_in_db: Maximum signal-to-noise ratio in db. A greater number means less noise.
         :param p: The probability of applying this transform
         """
         super().__init__(p)
         self.min_snr_in_db = min_snr_in_db
         self.max_snr_in_db = max_snr_in_db
+        if min_snr_in_db is None and max_snr_in_db is None:
+            # Apply legacy defaults
+            if min_SNR is None:
+                min_SNR = 0.001
+            if max_SNR is None:
+                max_SNR = 1.0
+        else:
+            if min_SNR is not None or max_SNR is not None:
+                raise Exception(
+                    "Error regarding AddGaussianSNR: Set min_snr_in_db"
+                    " and max_snr_in_db to None to keep using min_SNR and"
+                    " max_SNR parameters (legacy) instead. We highly recommend to use"
+                    " min_snr_in_db and max_snr_in_db parameters instead. To migrate"
+                    " from legacy parameters to new parameters,"
+                    " use the following conversion formulas: \n"
+                    "min_snr_in_db = -20 * math.log10(max_SNR)\n"
+                    "max_snr_in_db = -20 * math.log10(min_SNR)"
+                )
         self.min_SNR = min_SNR
         self.max_SNR = max_SNR
 
     def randomize_parameters(self, samples, sample_rate):
         super().randomize_parameters(samples, sample_rate)
         if self.parameters["should_apply"]:
-            std = np.std(samples)
+
             if self.min_SNR is not None and self.max_SNR is not None:
                 if self.min_snr_in_db is not None and self.max_snr_in_db is not None:
                     raise Exception(
-                        "Set min_snr_in_db and max_snr_in_db to None to keep using min_SNR and "
-                        "max_SNR parameters (legacy) instead. We highly recommend to use "
-                        "min_snr_in_db and max_snr_in_db parameters instead."
+                        "Error regarding AddGaussianSNR: Set min_snr_in_db"
+                        " and max_snr_in_db to None to keep using min_SNR and"
+                        " max_SNR parameters (legacy) instead. We highly recommend to use"
+                        " min_snr_in_db and max_snr_in_db parameters instead. To migrate"
+                        " from legacy parameters to new parameters,"
+                        " use the following conversion formulas: \n"
+                        "min_snr_in_db = -20 * math.log10(max_SNR)\n"
+                        "max_snr_in_db = -20 * math.log10(min_SNR)"
                     )
                 else:
                     warnings.warn(
-                        "You use legacy min_SNR and max_SNR parameters. "
-                        "We highly recommend to use min_snr_in_db and max_snr_in_db parameters instead."
+                        "You use legacy min_SNR and max_SNR parameters in AddGaussianSNR."
+                        " We highly recommend to use min_snr_in_db and max_snr_in_db parameters instead."
+                        " To migrate from legacy parameters to new parameters,"
+                        " use the following conversion formulas: \n"
+                        "min_snr_in_db = -20 * math.log10(max_SNR)\n"
+                        "max_snr_in_db = -20 * math.log10(min_SNR)"
                     )
                     min_snr = self.min_SNR
                     max_snr = self.max_SNR
+                    std = np.std(samples)
+                    self.parameters["noise_std"] = random.uniform(min_snr * std, max_snr * std)
             else:
-                min_snr = convert_decibels_to_amplitude_ratio(self.min_snr_in_db)
-                max_snr = convert_decibels_to_amplitude_ratio(self.max_snr_in_db)
+                # Pick snr in decibel scale
+                snr = random.uniform(self.min_snr_in_db, self.max_snr_in_db)
 
-            self.parameters["noise_std"] = random.uniform(min_snr * std, max_snr * std)
+                clean_rms = calculate_rms(samples)
+                noise_rms = calculate_desired_noise_rms(clean_rms=clean_rms, snr=snr)
+
+                # In gaussian noise, the RMS gets roughly equal to the std
+                self.parameters["noise_std"] = noise_rms
 
     def apply(self, samples, sample_rate):
-        if self.min_SNR is not None and self.max_SNR is not None:
-            noise = np.random.normal(
-                0.0, self.parameters["noise_std"], size=samples.shape
-            ).astype(np.float32)
-            return samples + noise
-        else:
-            if self.parameters["noise_std"] > 0:
-                noise = np.random.normal(
-                    0.0, 1.0 / self.parameters["noise_std"], size=samples.shape
-                ).astype(np.float32)
-                return samples + noise
-            else:
-                raise Exception("min_SNR or/and max_SNR must be positive")
+        noise = np.random.normal(
+            0.0, self.parameters["noise_std"], size=samples.shape
+        ).astype(np.float32)
+        return samples + noise
 
 
 class AddGaussianNoise(BaseWaveformTransform):
--- tests/test_gaussian_snr.py
+++ tests/test_gaussian_snr.py
@@ -1,38 +1,114 @@
 import json
+import math
 import unittest
 
 import numpy as np
+import random
+
+from audiomentations.core.utils import calculate_rms
 
 from audiomentations.augmentations.transforms import AddGaussianSNR
-from audiomentations.core.composition import Compose
 
 
 class TestGaussianSNR(unittest.TestCase):
-    def test_gaussian_noise_snr(self):
-        sample_len = 1024
-        samples_in = np.random.normal(0, 1, size=sample_len).astype(np.float32)
-        sample_rate = 16000
-        augmenter = Compose([AddGaussianSNR(min_snr_in_db=15, max_snr_in_db=35, p=1.0)])
+    def test_gaussian_noise_snr_defaults(self):
+        np.random.seed(42)
+        samples_in = np.random.normal(0, 1, size=1024).astype(np.float32)
+        augmenter = AddGaussianSNR(p=1.0)
         std_in = np.mean(np.abs(samples_in))
-        samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)
+        with self.assertWarns(UserWarning):
+            samples_out = augmenter(samples=samples_in, sample_rate=16000)
+        std_out = np.mean(np.abs(samples_out))
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertNotAlmostEqual(float(std_out), 0.0)
+        self.assertGreater(std_out, std_in)
+
+    def test_gaussian_noise_snr_legacy_positional_parameter(self):
+        np.random.seed(42)
+        samples_in = np.random.normal(0, 1, size=1024).astype(np.float32)
+        augmenter = AddGaussianSNR(0.001, 1.0, p=1.0)
+        std_in = np.mean(np.abs(samples_in))
+        with self.assertWarns(UserWarning):
+            samples_out = augmenter(samples=samples_in, sample_rate=16000)
+        std_out = np.mean(np.abs(samples_out))
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertNotAlmostEqual(float(std_out), 0.0)
+        self.assertGreater(std_out, std_in)
+
+    def test_gaussian_noise_snr_legacy_keyword_parameter(self):
+        np.random.seed(42)
+        samples_in = np.random.normal(0, 1, size=1024).astype(np.float32)
+        augmenter = AddGaussianSNR(min_SNR=0.001, max_SNR=1.0, p=1.0)
+        std_in = np.mean(np.abs(samples_in))
+        with self.assertWarns(UserWarning):
+            samples_out = augmenter(samples=samples_in, sample_rate=16000)
+        std_out = np.mean(np.abs(samples_out))
+        self.assertEqual(samples_out.dtype, np.float32)
+        self.assertNotAlmostEqual(float(std_out), 0.0)
+        self.assertGreater(std_out, std_in)
+
+    def test_gaussian_noise_snr_specify_both_new_and_legacy_params(self):
+        # Trying to specify both legacy and new parameters. This should raise an exception.
+        with self.assertRaises(Exception):
+            augmenter = AddGaussianSNR(
+                min_SNR=0.001, max_SNR=1.0, min_snr_in_db=15, max_snr_in_db=35, p=1.0
+            )
+
+    def test_gaussian_noise_snr(self):
+        np.random.seed(42)
+        samples_in = np.random.normal(0, 1, size=1024).astype(np.float32)
+        augmenter = AddGaussianSNR(min_snr_in_db=15, max_snr_in_db=35, p=1.0)
+        std_in = np.mean(np.abs(samples_in))
+        samples_out = augmenter(samples=samples_in, sample_rate=16000)
         std_out = np.mean(np.abs(samples_out))
         self.assertEqual(samples_out.dtype, np.float32)
         self.assertNotAlmostEqual(float(std_out), 0.0)
         self.assertGreater(std_out, std_in)
 
     def test_serialize_parameters(self):
+        np.random.seed(42)
         transform = AddGaussianSNR(min_snr_in_db=15, max_snr_in_db=35, p=1.0)
         samples = np.random.normal(0, 1, size=1024).astype(np.float32)
         transform.randomize_parameters(samples, sample_rate=16000)
         json.dumps(transform.serialize_parameters())
 
     def test_gaussian_noise_snr_multichannel(self):
+        np.random.seed(42)
         samples = np.random.normal(0, 0.1, size=(3, 8888)).astype(np.float32)
-        sample_rate = 16000
-        augmenter = Compose([AddGaussianSNR(min_snr_in_db=15, max_snr_in_db=35, p=1.0)])
-        samples_out = augmenter(samples=samples, sample_rate=sample_rate)
+        augmenter = AddGaussianSNR(min_snr_in_db=15, max_snr_in_db=35, p=1.0)
+        samples_out = augmenter(samples=samples, sample_rate=16000)
 
         self.assertEqual(samples_out.dtype, np.float32)
         self.assertGreater(
             float(np.sum(np.abs(samples_out))), float(np.sum(np.abs(samples)))
         )
+
+    def test_convert_old_parameters_to_new_parameters(self):
+        np.random.seed(42)
+        samples_in = np.random.normal(0, 1, size=1024).astype(np.float32)
+        min_SNR = 0.5
+        max_SNR = 0.5
+        min_snr_in_db = -20 * math.log10(min_SNR)
+        max_snr_in_db = -20 * math.log10(max_SNR)
+
+        self.assertAlmostEqual(min_snr_in_db, 6.0205999)
+        self.assertAlmostEqual(max_snr_in_db, 6.0205999)
+
+
+        legacy_augmenter = AddGaussianSNR(min_SNR=min_SNR, max_SNR=max_SNR, p=1.0)
+        new_augmenter = AddGaussianSNR(
+            min_snr_in_db=min_snr_in_db, max_snr_in_db=max_snr_in_db, p=1.0
+        )
+
+        np.random.seed(42)
+        random.seed(42)
+        with self.assertWarns(UserWarning):
+            samples_out_legacy = legacy_augmenter(samples=samples_in, sample_rate=16000)
+        np.random.seed(42)
+        random.seed(42)
+        samples_out_new = new_augmenter(samples=samples_in, sample_rate=16000)
+
+        legacy_rms = calculate_rms(samples_out_legacy)
+        new_rms = calculate_rms(samples_out_new)
+
+        self.assertAlmostEqual(legacy_rms, new_rms, places=3)

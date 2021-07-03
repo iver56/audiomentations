f4cf7afda51ccae451c46c8f04847e9d13679d68
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2021-07-03 18:55:33 +0300

    Update test_apply_wow_resampling.py

--- tests/test_apply_wow_resampling.py
+++ tests/test_apply_wow_resampling.py
@@ -9,7 +9,7 @@ class TestApplyWowResampling(unittest.TestCase):
     def test_single_channel(self):
         samples = np.array([0.5, 0.6, -0.2, 0.0], dtype=np.float32)
         sample_rate = 16000
-        augmenter = ApplyWowResampling(am=0.5, fm=0.5, p=1.0)
+        augmenter = ApplyWowResampling(min_am=0.5, max_am=1.5, min_fm=0.5, max_fm=1.5, p=1.0)
         samples = augmenter(samples=samples, sample_rate=sample_rate)
         
         self.assertEqual(samples.dtype, np.float32)
@@ -21,7 +21,7 @@ class TestApplyWowResampling(unittest.TestCase):
             dtype=np.float32,
         )
         sample_rate = 16000
-        augmenter = ApplyWowResampling(am=0.5, fm=0.5, p=1.0)
+        augmenter = ApplyWowResampling(min_am=0.5, max_am=1.5, min_fm=0.5, max_fm=1.5, p=1.0)
         samples = augmenter(samples=samples, sample_rate=sample_rate)
 
         self.assertEqual(samples.dtype, np.float32)

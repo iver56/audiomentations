7e16b63240e756cffbc49394fab4b73224e8f387
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2021-07-18 15:51:49 +0300

    Update test_low_pass_filter.py

--- tests/test_low_pass_filter.py
+++ tests/test_low_pass_filter.py
@@ -2,24 +2,27 @@ import unittest
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 from audiomentations.augmentations.transforms import LowPassFilter
 
 
 class TestLowPassFilter(unittest.TestCase):
     def test_low_pass_filter(self):
-        samples = np.array(
-            [
-                [[0.75, 0.5, -0.25, -0.125, 0.0], [0.65, 0.5, -0.25, -0.125, 0.0]],
-                [[0.3, 0.5, -0.25, -0.125, 0.0], [0.9, 0.5, -0.25, -0.125, 0.0]],
-                [[0.9, 0.5, -0.25, -1.06, 0.0], [0.9, 0.5, -0.25, -1.12, 0.0]],
-            ],
-            dtype=np.float32,
-        )
         sample_rate = 16000
+        t = .25                # signal duration in sec
+        f = 500                # signal frequency in Hz
+        samples = np.arange(t * f, dtype=np.float32) / sample_rate
+        samples = np.sin(2 * np.pi * f * samples)
 
-        augment = LowPassFilter(min_cutoff_freq=200, max_cutoff_freq=7000, p=1.0)
+        augment = LowPassFilter(min_cutoff_freq=100, max_cutoff_freq=200, p=1.0)
         processed_samples = augment(
             samples=samples, sample_rate=sample_rate
         )
+
         self.assertEqual(processed_samples.shape, samples.shape)
         self.assertEqual(processed_samples.dtype, np.float32)
+        
+        plt.plot(samples)
+        plt.plot(processed_samples, '-.')
+        plt.show()

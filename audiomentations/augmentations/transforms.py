6cbe1dbacfcaab55589c2e22cfafdb1aee954a0b
Cangonin <celine.angonin@nomono.co>
2021-10-18 16:20:52 +0200

    Added an option "absolute" to the transform AddBackgroundNoise so that the added background noise can have a noise rms independent of the rms of the input audio. Added also a test to this option but this is maybe not needed. :)

--- audiomentations/augmentations/transforms.py
+++ audiomentations/augmentations/transforms.py
@@ -766,8 +766,8 @@ class AddBackgroundNoise(BaseWaveformTransform):
     ideally be at least as long as the input sounds to be transformed. Otherwise, the background
     sound will be repeated, which may sound unnatural.
 
-    Note that the gain of the added noise is relative to the amount of signal in the input. This
-    implies that if the input is completely silent, no noise will be added.
+    Note that the gain of the added noise is relative to the amount of signal in the input if the parameter noise_rms
+    is set to "relative" (default option). This implies that if the input is completely silent, no noise will be added.
 
     Here are some examples of datasets that can be downloaded and used as background noise:
 
@@ -778,6 +778,9 @@ class AddBackgroundNoise(BaseWaveformTransform):
     def __init__(
         self,
         sounds_path=None,
+        noise_rms="relative",
+        min_absolute_rms_in_db=-45,
+        max_absolute_rms_in_db=-15,
         min_snr_in_db=3,
         max_snr_in_db=30,
         p=0.5,
@@ -786,16 +789,33 @@ class AddBackgroundNoise(BaseWaveformTransform):
         """
         :param sounds_path: Path to a folder that contains sound files to randomly mix in. These
             files can be flac, mp3, ogg or wav.
-        :param min_snr_in_db: Minimum signal-to-noise ratio in dB
-        :param max_snr_in_db: Maximum signal-to-noise ratio in dB
+        :param noise_rms: Defines how the background noise will be added to the audio input. If the chosen
+            option is "relative", the rms of the added noise will be proportional to the rms of
+            the input sound. If the chosen option is "absolute", the background noise will have
+            a rms independent of the rms of the input audio file. The default option is "relative".
+        :param min_absolute_rms_in_db: Is only used if noise_rms is set to "absolute". It is
+            the minimum rms value in dB that the added noise can take. The lower the rms is, the
+            lower will the added sound be.
+        :param max_absolute_rms_in_db: Is only used if noise_rms is set to "absolute". It is
+            the maximum rms value in dB that the added noise can take. Note that this value
+            can not exceed 0.
+        :param min_snr_in_db: Minimum signal-to-noise ratio in dB. Is only used if noise_rms is set to "relative"
+        :param max_snr_in_db: Maximum signal-to-noise ratio in dB. Is only used if noise_rms is set to "relative"
         :param p: The probability of applying this transform
         :param lru_cache_size: Maximum size of the LRU cache for storing noise files in memory
         """
         super().__init__(p)
         self.sound_file_paths = get_file_paths(sounds_path)
         self.sound_file_paths = [str(p) for p in self.sound_file_paths]
+
+        assert min_absolute_rms_in_db <= max_absolute_rms_in_db <= 0
+        assert min_snr_in_db <= max_snr_in_db
         assert len(self.sound_file_paths) > 0
+
+        self.noise_rms = noise_rms
         self.min_snr_in_db = min_snr_in_db
+        self.min_absolute_rms_in_db = min_absolute_rms_in_db
+        self.max_absolute_rms_in_db = max_absolute_rms_in_db
         self.max_snr_in_db = max_snr_in_db
         self._load_sound = functools.lru_cache(maxsize=lru_cache_size)(
             AddBackgroundNoise._load_sound
@@ -810,6 +830,9 @@ class AddBackgroundNoise(BaseWaveformTransform):
         if self.parameters["should_apply"]:
             self.parameters["snr_in_db"] = random.uniform(
                 self.min_snr_in_db, self.max_snr_in_db
+            )
+            self.parameters["rms_in_db"] = random.uniform(
+                self.min_absolute_rms_in_db, self.max_absolute_rms_in_db
             )
             self.parameters["noise_file_path"] = random.choice(self.sound_file_paths)
 
@@ -845,12 +868,22 @@ class AddBackgroundNoise(BaseWaveformTransform):
             return samples
 
         clean_rms = calculate_rms(samples)
-        desired_noise_rms = calculate_desired_noise_rms(
-            clean_rms, self.parameters["snr_in_db"]
-        )
 
-        # Adjust the noise to match the desired noise RMS
-        noise_sound = noise_sound * (desired_noise_rms / noise_rms)
+        if self.noise_rms == "relative":
+            desired_noise_rms = calculate_desired_noise_rms(
+                clean_rms, self.parameters["snr_in_db"]
+            )
+
+            # Adjust the noise to match the desired noise RMS
+            noise_sound = noise_sound * (desired_noise_rms / noise_rms)
+
+        if self.noise_rms == "absolute":
+            desired_noise_rms_db = self.parameters["rms_in_db"]
+            desired_noise_rms_amp = convert_decibels_to_amplitude_ratio(
+                desired_noise_rms_db
+            )
+            gain = desired_noise_rms_amp / noise_rms
+            noise_sound = noise_sound * gain
 
         # Repeat the sound if it shorter than the input sound
         num_samples = len(samples)

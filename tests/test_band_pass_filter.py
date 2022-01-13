fac10314476333d68b5064bb5973d01312c457ff
Emmanouil Theofanis Chourdakis <emmanouil.chourdakis@nomono.co>
2022-01-13 15:17:32 +0100

    Add magnitude tests for low/high/band pass filters

--- tests/test_band_pass_filter.py
+++ tests/test_band_pass_filter.py
@@ -1,7 +1,7 @@
 import unittest
 
 import numpy as np
-
+import scipy
 import matplotlib.pyplot as plt
 
 from audiomentations import BandPassFilter
@@ -11,21 +11,55 @@ DEBUG = False
 
 class TestBandPassFilter(unittest.TestCase):
     def test_band_pass_filter(self):
-        sample_rate = 16000
-        t = 0.25  # signal duration in sec
-        f = 500  # signal frequency in Hz
-        samples = np.arange(t * f, dtype=np.float32) / sample_rate
-        samples = np.sin(2 * np.pi * f * samples)
 
-        augment = BandPassFilter(
-            min_center_freq=100, max_center_freq=5000, min_q=1.0, max_q=2.0, p=1.0
-        )
+        np.random.seed(1)
+        sample_rate = 8000
+
+        # Filter cutoff
+        fc_lowpass = 3000
+
+        # Parameters for computing periodograms
+        nfft = 1024
+        nperseg = 1024
+
+        # Create a 20 seconds chirp from 50Hz to 250Hz
+        n = np.arange(0, 20 , 1/sample_rate)
+        samples = scipy.signal.chirp(n, 50, 20, 2000, method='linear')
+
+        augment = BandPassFilter( min_center_freq=100.0, max_center_freq=1000.0, min_q=1.0, max_q=2.0, p=1.0)
         processed_samples = augment(samples=samples, sample_rate=sample_rate)
+        fcL = augment.parameters['center_freq'] * (1 - 0.5 / augment.parameters["q"])
+        fcH = augment.parameters['center_freq'] * (1 + 0.5 / augment.parameters["q"])
+
+
+        # Compute periodograms
+        wx, samples_pxx = scipy.signal.welch(samples, fs=sample_rate, nfft=nfft, nperseg=nperseg, scaling='spectrum',  window='hann')
+        _, processed_samples_pxx = scipy.signal.welch(processed_samples, fs=sample_rate, nperseg=nperseg, nfft=nfft, scaling='spectrum', window='hann')
+
+        # Compute db at cutoffs between the ideal
+        samples_db_at_fcL = 10*np.log10(samples_pxx[int(np.round(nfft/sample_rate * fcL))])
+        processed_samples_db_at_fcL = 10*np.log10(processed_samples_pxx[int(np.round(nfft/sample_rate * fcL))])
+
+        samples_db_at_fcH = 10*np.log10(samples_pxx[int(np.round(nfft/sample_rate * fcH))])
+        processed_samples_db_at_fcH = 10*np.log10(processed_samples_pxx[int(np.round(nfft/sample_rate * fcH))])        
+
+
+        if DEBUG:
+            plt.plot(wx, 10*np.log10(np.abs(samples_pxx)))
+            plt.plot(wx, 10*np.log10(np.abs(processed_samples_pxx)), ':')
+            plt.legend(['Input signal', f'Bandpassed at f_l={fcL:.2f}, f_h={fcH:.2f}'])
+            plt.axvline(fcL, color='red', linestyle=':')
+            plt.axvline(fcH, color='red', linestyle=':')
+            plt.axhline(samples_db_at_fcL - 3, color='red',  linestyle=':')
+            plt.axhline(samples_db_at_fcH - 3, color='red',  linestyle='-.')
+            plt.xlabel('Frequency (Hz)')
+            plt.ylabel('Magnitude (dB)')
+            plt.show()
 
         self.assertEqual(processed_samples.shape, samples.shape)
         self.assertEqual(processed_samples.dtype, np.float32)
+        
+        # Assert that at fc we are at the 3db point give or take half a db.
+        self.assertTrue(np.isclose(samples_db_at_fcL - processed_samples_db_at_fcL, 3, 0.5))
+        self.assertTrue(np.isclose(samples_db_at_fcH - processed_samples_db_at_fcH, 3, 0.5))
 
-        if DEBUG:
-            plt.plot(samples)
-            plt.plot(processed_samples, "-.")
-            plt.show()
--- tests/test_high_pass_filter.py
+++ tests/test_high_pass_filter.py
@@ -1,5 +1,6 @@
 import unittest
-
+import scipy
+import scipy.signal
 import numpy as np
 
 import matplotlib.pyplot as plt
@@ -11,19 +12,48 @@ DEBUG = False
 
 class TestHighPassFilter(unittest.TestCase):
     def test_high_pass_filter(self):
-        sample_rate = 16000
-        t = 0.25  # signal duration in sec
-        f = 500  # signal frequency in Hz
-        samples = np.arange(t * f, dtype=np.float32) / sample_rate
-        samples = np.sin(2 * np.pi * f * samples)
 
-        augment = HighPassFilter(min_cutoff_freq=100, max_cutoff_freq=200, p=1.0)
+        np.random.seed(1)
+
+        sample_rate = 8000
+
+        # Filter cutoff
+        fc_lowpass = 3000
+
+        # Parameters for computing periodograms
+        nfft = 1024
+        nperseg = 1024
+
+        # Create a 20 seconds chirp from 50Hz to 250Hz
+        n = np.arange(0, 20 , 1/sample_rate)
+        samples = scipy.signal.chirp(n, 550, 20, 2000, method='linear')
+
+        augment = HighPassFilter(min_cutoff_freq=600, max_cutoff_freq=1000, p=1.0)
         processed_samples = augment(samples=samples, sample_rate=sample_rate)
+        fc = augment.parameters['cutoff_freq']
+
+        # Compute periodograms
+        wx, samples_pxx = scipy.signal.welch(samples, fs=sample_rate, nfft=nfft, nperseg=nperseg, scaling='spectrum',  window='hann')
+        _, processed_samples_pxx = scipy.signal.welch(processed_samples, fs=sample_rate, nperseg=nperseg, nfft=nfft, scaling='spectrum', window='hann')
+
+        # Compute db at cutoffs between the ideal
+        samples_db_at_fc = 10*np.log10(samples_pxx[int(np.round(nfft/sample_rate * fc))])
+        processed_samples_db_at_fc = 10*np.log10(processed_samples_pxx[int(np.round(nfft/sample_rate * fc))])
+
+        if DEBUG:
+            plt.plot(wx, 10*np.log10(np.abs(samples_pxx)))
+            plt.plot(wx, 10*np.log10(np.abs(processed_samples_pxx)), ':')
+            plt.legend(['Input signal', f'Highpassed at f_c={fc:.2f}'])
+            plt.axvline(fc, color='red', linestyle=':')
+            plt.axhline(samples_db_at_fc-3, color='red',  linestyle=':')
+            plt.xlabel('Frequency (Hz)')
+            plt.ylabel('Magnitude (dB)')
+            plt.show()
+
 
         self.assertEqual(processed_samples.shape, samples.shape)
         self.assertEqual(processed_samples.dtype, np.float32)
+        
+        # Assert that at fc we are at the 3db point give or take half a db.
+        self.assertTrue(np.isclose(samples_db_at_fc - processed_samples_db_at_fc, 3, 0.5))
 
-        if DEBUG:
-            plt.plot(samples)
-            plt.plot(processed_samples, "-.")
-            plt.show()

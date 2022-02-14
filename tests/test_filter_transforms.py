b2f0bcaee63cb61899317d43038e918598ab443d
iver56 <iver56@hotmail.com>
2022-02-14 13:37:17 +0100

    Use float32 inputs instead of float64 as audio input in filter tests

--- tests/test_filter_transforms.py
+++ tests/test_filter_transforms.py
@@ -16,25 +16,17 @@ DEBUG = False
 
 
 def get_chirp_test(sample_rate, duration):
-    """
-    Create a `duration` seconds chirp from 0Hz to `nyquist frequency`
-    """
-
+    """Create a `duration` seconds chirp from 0Hz to `nyquist frequency`"""
     n = np.arange(0, duration, 1 / sample_rate)
     samples = scipy.signal.chirp(n, 0, duration, sample_rate // 2, method="linear")
-    return samples
+    return samples.astype(np.float32)
 
 
 def get_randn_test(sample_rate, duration):
-    """
-    Create a random noise test stimulus
-    """
-
+    """Create a random noise test stimulus"""
     n_samples = int(duration * sample_rate)
-
     samples = np.random.randn(n_samples)
-
-    return samples
+    return samples.astype(np.float32)
 
 
 class TestPeakingFilterTransforms:

995e8a666711cf8f91a0578a2179228eb69d7ba4
iver56 <iver56@hotmail.com>
2022-05-23 08:49:56 +0200

    Try to explain how GainTransition works

--- audiomentations/augmentations/gain_transition.py
+++ audiomentations/augmentations/gain_transition.py
@@ -33,6 +33,14 @@ class GainTransition(BaseWaveformTransform):
     Gradually change the volume up or down over a random time span. Also known as
     fade in and fade out. The fade works on a logarithmic scale, which is natural to
     human hearing.
+
+    The way this works is that it picks two gains: a first gain and a second gain.
+    Then it picks a time range for the the transition between those two gains.
+    Note that this transition can start before the audio starts and/or end after the
+    audio ends, so the output audio can start or end in the middle of a transition.
+    The gain starts at the first gain and is held constant until the transition start.
+    Then it transitions to the second gain. Then that gain is held constant until the
+    end of the sound.
     """
 
     supports_multichannel = True

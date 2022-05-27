e0efc423947eea2ac3baf9f4b446b9b9722d18d6
Araik Tamazian <9746588+atamazian@users.noreply.github.com>
2022-05-27 14:47:38 +0400

    another error fix

--- audiomentations/core/utils.py
+++ audiomentations/core/utils.py
@@ -16,6 +16,32 @@ SUPPORTED_EXTENSIONS = (
     ".opus",
     ".wav",
 )
+
+def find_audio_files(
+    root_path,
+    filename_endings=SUPPORTED_EXTENSIONS,
+    traverse_subdirectories=True,
+    follow_symlinks=True,
+):
+    """Return a list of paths to all audio files with the given extension(s) in a directory.
+    Also traverses subdirectories by default.
+    """
+    file_paths = []
+
+    for root, dirs, filenames in os.walk(root_path, followlinks=follow_symlinks):
+        filenames = sorted(filenames)
+        for filename in filenames:
+            input_path = os.path.abspath(root)
+            file_path = os.path.join(input_path, filename)
+
+            if filename.lower().endswith(filename_endings):
+                file_paths.append(Path(file_path))
+        if not traverse_subdirectories:
+            # prevent descending into subfolders
+            break
+
+    return file_paths
+
     
 def find_audio_files_in_paths(
     paths: Union[List[Path], List[str], Path, str],
@@ -40,7 +66,7 @@ def find_audio_files_in_paths(
             file_path = Path(os.path.abspath(p))
             file_paths.append(file_path)
         elif os.path.isdir(p):
-            file_paths += find_audio_files_in_paths(
+            file_paths += find_audio_files(
                 p,
                 filename_endings=filename_endings,
                 traverse_subdirectories=traverse_subdirectories,

756fa62a491a86ecd51b2b10edf1fe65df134a54
TusharDhyani <dhyanitushar@gmail.com>
2022-08-03 13:47:41 +0200

    Added dependency on sample_rate in lambda operator

--- audiomentations/augmentations/fnlambda.py
+++ audiomentations/augmentations/fnlambda.py
@@ -11,20 +11,15 @@ class Lambda(BaseWaveformTransform):
 
     supports_multichannel = True
 
-    def __init__(
-        self: BaseWaveformTransform, 
-        operator: Callable, 
-        p: int = 0.5, 
-        **kwargs: dict
-    ):
+    def __init__(self: BaseWaveformTransform, operator: Callable, p: int = 0.5, **kwargs: dict):
         """
         :param operator: A callable to be applied over samples
         :param p: The probability of applying this transform
         :param **kwrags: The parameters and the values to be passed to the operator.
         """
-        super().__init__(p = p)
+        super().__init__(p=p)
         self.operator = operator
         self.kwargs = kwargs
 
     def apply(self, samples, sample_rate):
-        return self.operator(samples, **self.kwargs)
+        return self.operator(samples, sample_rate, **self.kwargs)

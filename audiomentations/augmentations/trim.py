d9f39f849b6b29bfd1cc32f7dce23b4434ca3840
iver56 <iver56@hotmail.com>
2022-09-13 10:17:40 +0200

    Switch to new defaults in Trim. Close #179

--- audiomentations/augmentations/trim.py
+++ audiomentations/augmentations/trim.py
@@ -1,6 +1,3 @@
-import warnings
-from typing import Optional
-
 import librosa
 
 from audiomentations.core.transforms_interface import BaseWaveformTransform
@@ -13,29 +10,11 @@ class Trim(BaseWaveformTransform):
 
     supports_multichannel = True
 
-    def __init__(self, top_db: Optional[float] = None, p: Optional[float] = None):
+    def __init__(self, top_db: float = 30.0, p: float = 0.5):
         """
         :param top_db: The threshold (in decibels) below reference to consider as silence
         :param p: The probability of applying this transform
         """
-        if top_db is None:
-            top_db = 20
-            warnings.warn(
-                "The default value of top_db in Trim will change from 20 to 30 in a future"
-                " version of audiomentations. Please specify it explicitly to make your"
-                " code more future-proof and to get rid of this warning.",
-                FutureWarning
-            )
-
-        if p is None:
-            p = 1.0
-            warnings.warn(
-                "The default value of p in Trim will change from 1.0 to 0.5 in a future"
-                " version of audiomentations. Please specify it explicitly to make your"
-                " code more future-proof and to get rid of this warning.",
-                FutureWarning
-            )
-
         super().__init__(p)
         self.top_db = top_db
 

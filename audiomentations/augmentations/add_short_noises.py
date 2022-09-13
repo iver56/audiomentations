ebc97ceb08ca572d1b3a0d1cfbc39dc39e1d603d
Céline Angonin <celine.angonin@nomono.co>
2022-09-13 09:12:23 +0200

    Revert and add warning

--- audiomentations/augmentations/add_short_noises.py
+++ audiomentations/augmentations/add_short_noises.py
@@ -31,7 +31,7 @@ class AddShortNoises(BaseWaveformTransform):
         max_snr_in_db: float = 24.0,
         min_time_between_sounds: float = 4.0,
         max_time_between_sounds: float = 16.0,
-        noise_rms: str = "relative_to_whole_input",
+        noise_rms: str = "relative",
         min_absolute_noise_rms_db: float = -50.0,
         max_absolute_noise_rms_db: float = -20,
         add_all_noises_with_same_level: bool = False,
@@ -130,6 +130,14 @@ class AddShortNoises(BaseWaveformTransform):
         assert min_absolute_noise_rms_db <= max_absolute_noise_rms_db < 0
         assert type(include_silence_in_noise_rms_estimation) == bool
         assert noise_rms in ["relative", "absolute", "relative_to_whole_input"]
+
+        if noise_rms == "relative":
+            warnings.warn(
+                "The default value of 'noise_rms' will change from 'relative' to"
+                "'relative_to_whole_input' in a future version of audiomentations."
+                "Please specify explicitly 'noise_rms' to 'relative' if you don't want to change"
+                "its current behaviour. Discard this warning if you explicitly provided 'noise_rms'."
+            )
 
         self.min_snr_in_db = min_snr_in_db
         self.max_snr_in_db = max_snr_in_db

a8ac7bad81b053d32f5503abe9df872b36dda997
BakerBunker <bakerbunker@mail.nwpu.edu.cn>
2023-03-18 18:59:47 +0800

    Add DurationClip

---
+++ audiomentations/augmentations/duration_clip.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+from audiomentations.core.transforms_interface import BaseWaveformTransform
+
+
+class DurationClip(BaseWaveformTransform):
+    """
+    Clip audio by duration specified in samples or seconds.
+    If the input sound is longer than the target duration,
+    pick a random offset and crop the sound to the target duration.
+    If the input sound is shorter than the target duration,
+    pad the end of the sound (append digital silence) so the duration matches the target duration.
+    """
+
+    supports_multichannel=True
+
+    def __init__(
+        self,
+        duration_samples: int = None,
+        duration_seconds: float = None,
+        pad_mode: str = "constant",
+        pad_section: str = "end",
+        p: float = 0.5,
+    ):
+        """
+        :param duration_samples: Target duratin in sample length
+        :param duration_seconds: Target duratin in second
+        :param pad_mode: Padding mode. Must be supported by `numpy.pad`
+        :param pad_section: Which part of the signal should be replaced with padding:
+            "start" or "end"
+        :param p: The probability of applying this transform
+        """
+        super().__init__(p)
+        self.mode = pad_mode
+
+        assert pad_section in ("start", "end")
+        self.pad_section = pad_section
+
+        assert duration_samples is not None or duration_seconds is not None
+        if duration_samples is not None and duration_seconds is not None:
+            raise ValueError(
+                "should have duration_samples or duration_seconds, but not both"
+            )
+        elif duration_seconds:
+            assert duration_seconds > 0
+            self.get_target_samples = lambda sr: int(duration_seconds * sr)
+        elif duration_samples:
+            assert duration_samples > 0
+            self.get_target_samples = lambda sr: duration_samples
+
+    def apply(self, samples: np.ndarray, sample_rate: int):
+        target_samples = self.get_target_samples(sample_rate)
+        sample_length = samples.shape[-1]
+
+        if sample_length == target_samples:
+            return samples
+
+        elif sample_length > target_samples:
+            start = np.random.randint(0, sample_length - target_samples)
+            return samples[..., start : start + target_samples]
+
+        elif sample_length < target_samples:
+            padding_length = target_samples - sample_length
+            if samples.ndim == 1:
+                if self.pad_section == "start":
+                    pad_width = (padding_length, 0)
+                else:
+                    pad_width = (0, padding_length)
+            else:
+                if self.pad_section == "start":
+                    pad_width = ((0, 0), (padding_length, 0))
+                else:
+                    pad_width = ((0, 0), (0, padding_length))
+            return np.pad(samples, pad_width, self.mode)

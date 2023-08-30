4cc7e6cbf055b31dd01bac9bd00895b1aa5f8a22
iver56 <iver56@hotmail.com>
2023-08-30 13:56:27 +0200

    Bump min numpy version to 1.21 for numpy.typing support

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     url="https://github.com/iver56/audiomentations",
     packages=find_packages(exclude=["demo", "tests"]),
     install_requires=[
-        "numpy>=1.18.0",
+        "numpy>=1.21.0",
         "librosa>=0.8.0,!=0.10.0,<0.11.0",
         "scipy>=1.4.0,<2",
         "soxr>=0.3.2,<1.0.0",

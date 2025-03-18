45c53e46d40537a29a9705f2d315d89e365cd1d1
iver56 <iver56@users.noreply.github.com>
2025-03-18 14:18:14 +0100

    Remove Python 3.8 support

--- setup.py
+++ setup.py
@@ -54,9 +54,8 @@ setup(
             "pyroomacoustics>=0.7.4",
         ]
     },
-    python_requires=">=3.8,<3.13",
+    python_requires=">=3.9,<3.13",
     classifiers=[
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",

ce2e6a1adc43b44df9c19c544b65824d06802c30
iver56 <iver56@hotmail.com>
2025-03-20 12:08:05 +0100

    Remove unneeded Compose in test_trim

--- tests/test_trim.py
+++ tests/test_trim.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from audiomentations import Trim, Compose
+from audiomentations import Trim
 
 
 def test_trim():
@@ -8,7 +8,7 @@ def test_trim():
     samples1 = np.zeros((sample_len,), dtype=np.float32)
     samples2 = np.random.normal(0, 1, size=sample_len).astype(np.float32)
     sample_rate = 16000
-    augmenter = Compose([Trim(top_db=20, p=1.0)])
+    augmenter = Trim(top_db=20, p=1.0)
     samples_in = np.hstack((samples1, samples2))
     assert len(samples_in) == sample_len * 2
     samples_out = augmenter(samples=samples_in, sample_rate=sample_rate)

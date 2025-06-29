781e5ddb9f662fbbfdd3eab2141651d075349899
iver56 <iver56@hotmail.com>
2025-06-29 20:28:54 +0200

    Remove deprecated license classifier

--- setup.py
+++ setup.py
@@ -61,7 +61,6 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",

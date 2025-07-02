d346ad30b8cde9d6ef263e39184fcd1a13b1af80
iver56 <iver56@hotmail.com>
2025-07-02 23:31:17 +0200

    Add upper version limit

--- setup.py
+++ setup.py
@@ -47,9 +47,9 @@ setup(
     ],
     extras_require={
         "extras": [
-            "fast_mp3_augment",
+            "fast_mp3_augment<1",
             "lameenc>=1.2.0,<2",
-            "numpy-audio-limiter",
+            "numpy-audio-limiter<1",
             "pydub>=0.22.0,<1",
             "pyloudnorm>=0.1.0",
             "pyroomacoustics>=0.7.4",
